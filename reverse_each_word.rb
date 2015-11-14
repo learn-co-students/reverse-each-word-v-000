4aaa9256f3ba9245096be2c4d356cfa6b10ae293
mmmeyers <marsi286@gmail.com>
2015-11-14 15:08:16 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  new_sentence = sentence.split
+  new_sentence.each do |word|
+    word.reverse!
+  end
+  new_sentence.join(" ")
+end
+
+def reverse_each_word(sentence)
+  new_sentence = sentence.split
+  new_sentence.collect do |word|
+    word.reverse!
+  end
+  new_sentence.join(" ")
+end
