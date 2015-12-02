7cf13042671bf0b738cd5d019c305312defc1167
yoshikot <ytamaoki@gmail.com>
2015-12-01 23:57:23 -0500

    Done.

--- reverse_each_word-v1.rb
+++ reverse_each_word-v1.rb
@@ -1,3 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  reversed_word_array = sentence.split(" ").collect do |word|
+    word.reverse!
+  end
+  reversed_word_array.join(" ")
 end
