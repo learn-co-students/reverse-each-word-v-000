536195411a8cd9526de75d3b33c332be6693fd79
Shmuwol <shmully.wolfson@gmail.com>
2015-12-15 21:44:10 -0500

    refactor method using collect

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,9 +8,11 @@
 # end
 
 def reverse_each_word(sentence)
-	sentence = sentence.split(" ")
-	sentence.collect do |word|
-		word.reverse!
+	string_array = sentence.split(" ")
+	string_array.collect do |word|
+		word.reverse
 	end
-  sentence.join(" ")
+	.join(" ")
 end
+
+p reverse_each_word("Hello there, and how are you?")
