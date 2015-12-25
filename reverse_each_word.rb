5cb8ccff5e3cfd0de1ad5d6af2fb5f6a60bc5a0b
Brad1974 <danieltbrad@gmail.com>
2015-12-25 14:22:25 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
-
 def reverse_each_word(sentence)
   array = sentence.split
   new_array = []
   array.collect do |word|
-    x = word.reverse 
-    new_array <<(x)
+    new_array << word.reverse
   end
   new_array.join(" ")
 end
+
