cfdd60d88cb42205f4f7525b7c9f9c709173ad3a
nicolefederici <nicolefederici@yahoo.com>
2016-03-14 16:35:54 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   array = sentence.split
-  reverse_array = array.collect {|item| item.reverse}
-  return reverse_array.join(" ")
+  array.collect! {|item| item.reverse}
+  array.join(" ")
 end
 
