dbf157797024b659ffebec6a7dde6f435165e0b5
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-03-22 15:50:00 -0700

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,8 @@
 def reverse_each_word(string)
+	string.split.collect {|word| word.reverse}.join(" ")
+end
+
+def reverse_each_word(string)
 	array = []
 	string.split.each {|word| array << word.reverse}
 	return array.join(" ")
