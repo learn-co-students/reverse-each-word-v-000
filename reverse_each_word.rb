f43e100ac1c0386fb786552524d3b6a58b332450
m-walker <michaelwalker610@gmail.com>
2016-04-03 18:31:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,5 @@
 def reverse_each_word(sentence)
   array_of_words = sentence.split(" ")
-  new_array_of_words = Array.new
-  array_of_words.each do |x|
-    new_array_of_words << x.reverse
-  end
-    new_array_of_words.join(" ")
+  array_of_words.map!{|x| x.reverse}
+  array_of_words.join(" ")
 end
-
-reverse_each_word("This is a sentence.")
