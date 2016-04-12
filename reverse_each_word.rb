0940b63ff59e8d477ab858aa5f0e0aa0e91ba364
msosborne3 <msosborne@ymail.com>
2016-04-11 21:29:28 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,8 @@
-#def reverse_each_word(sentence)
-#  words = sentence.split(" ")
-#  words.each do |word|
-#    word.reverse!
-#  end
-#
-#  words.join(" ")
-#end
-
-
 def reverse_each_word(sentence)
   words = sentence.split(" ")
-  words.map do |word|
-    word.reverse
+  words.each do |word|
+    word.reverse!
   end
+
   words.join(" ")
 end
