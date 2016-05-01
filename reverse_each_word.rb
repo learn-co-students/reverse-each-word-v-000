7bdf91780590c76b10eef9a04bced97aece3d574
Being Young <beingy@gmail.com>
2016-05-01 19:12:28 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,3 @@
 def reverse_each_word(sentence1)
-  sentence1.reverse!
-  sentence2=sentence1.split(" ")
-  sentence2.reverse.join(" ")
+  sentence1.split(" ").collect {|word| word.reverse}.join(" ")
 end
