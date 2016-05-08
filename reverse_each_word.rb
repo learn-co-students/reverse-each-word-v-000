8e9767d1961c6cbdb9da0940e5e6c25ab58c3eb5
hilaryml <hilyttle@gmail.com>
2016-05-07 23:11:41 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,17 @@
+def reverse_each_word(string)   #method using collect
+	string.split.collect {|backwards| backwards.reverse}.join(" ")  
+end
+
+#.split converts the string into an array
+#.collect iterates over the array and alters it based on the block
+#.reverse reverses each element in the array
+#.join turns the array into a string again
+
+
 #def reverse_each_word(string)  #method using each
 #	array = string.split(" ") #turns string into array
 #	backwards_array = []  #new empty array for backwards words
-#	array.each {|backwards|  
-#		backwards_array << backwards.reverse  #iterates over each element in array and reverses it, then shovels it into the backwards array 
-#	}
+#	array.each {|backwards| backwards_array << backwards.reverse}  #block iterates over each element in array and reverses it, then shovels it into the backwards array 
 #	backwards_array.join(" ")  #converts the backwards array into a string and returns it
 #end
 
-def reverse_each_word(string)   #method using collect
-	array = string.split(" ") #turns string into array
-	array.collect {|backwards|  
-		backwards.reverse!  #iterates over each element in array and reverses it
-	}
-	array.join(" ")  #converts the array into a string and returns it
-end
