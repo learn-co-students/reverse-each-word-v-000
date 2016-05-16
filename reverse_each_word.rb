85ada25f378801e5421dd68057a01bacdf9782eb
Savannah Scott <sascott624@gmail.com>
2016-05-16 15:02:52 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,14 @@
+#def reverse_each_word(string)
+#  array = []
+#  (string.split(" ")).each do |word|
+#    array << word.reverse
+#  end
+#  return array.join(" ")
+#end
+
 def reverse_each_word(string)
-  array = []
-  (string.split(" ")).each do |word|
-    array << word.reverse
+  array = (string.split(" ")).collect do |word|
+    word.reverse
   end
-  return array.join(" ")
+  array.join(" ")
 end
