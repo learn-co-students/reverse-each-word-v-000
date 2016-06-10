5150654683092b0da93427aa15d4bfb0ee6f608d
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-10 16:13:20 +0000

    After passing with #collect iterator

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
     string = string.split(" ")
-    new_arr = []
-    string.each do |word|
-       new_arr << word.reverse! 
+    string.collect do |word|
+       word.reverse! 
     end
-    new_arr.join(" ")
+    string.join(" ")
 end
