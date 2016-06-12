9e5e49905a2f5db036929dc0012e42687f46697d
bging4 <wgingher@gmail.com>
2016-06-12 12:14:57 -0400

    Complete with each and map methods

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,3 @@
 def reverse_each_word(sentence)
-  new_sentence = []
-  reversed_sentence = sentence.split
-  reversed_sentence.each do |word|
-    new_sentence << word.reverse
-  end
-  new_sentence.join(" ")
+  sentence.split.map {|word| word.reverse}.join(" ")
 end
