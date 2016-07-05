b9e11b8c63c4870ba65f600bf921b05f330073f5
ga-be <gj@mail.co.de>
2016-07-05 16:42:16 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
+# def reverse_each_word(sentence)
+#   word_array = sentence.split(" ")
+#   word_array.each {|word| word.reverse!}
+#   sentence = word_array.join(" ")
+# end
+
 def reverse_each_word(sentence)
   word_array = sentence.split(" ")
-  word_array.each {|word| word.reverse!}
+  word_array.collect {|word| word.reverse!}
   sentence = word_array.join(" ")
 end
--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -5,11 +5,14 @@ describe '#reverse_each_word' do
   it 'reverses all the words in a string without reversing the order of the words' do
     expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
   end
-end
 
-describe '#reverse_each_word' do
   let(:sentence2) { "Hi again, just making sure it's reversed!" }
   it 'reverses all the words in a string without reversing the order of the words' do
     expect(reverse_each_word(sentence2)).to eq("iH ,niaga tsuj gnikam erus s'ti !desrever")
+  end
+
+  let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'reverse_each_word.rb')) }
+  it 'uses .collect' do
+    expect(file_contents).to include(".collect")
   end
 end
