c90568bdfbfaa51b73b7598f60d5f1b87a03457a
vanita eckert <vanita112003@gmail.com>
2016-07-06 17:57:25 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  sentence = sentence.split
-  sentence.collect do |each_word|
-binding.pry
+  sentence.split.collect do |each_word|
     each_word.reverse
+  end.join(" ")
 end
-end
+
+#  def reverse_each_word(sentence)
+#   new_sentence = []
+#   sentence.split.each do |each_word|
+#     new_sentence << each_word.reverse
+#   end
+#     new_sentence.join(" ")
+#  end
