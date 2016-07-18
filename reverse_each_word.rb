f2fc833d6c744db480272726b0a38e67b8e03ac7
Christina Cole <christinal.cole@gmail.com>
2016-07-18 18:39:10 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  #arr=string.split
+  arr=string.split
 
-  #new_arr=[]
+  new_arr=[]
 
   #version of method using .each
   #arr.each do |word|
@@ -10,9 +10,8 @@ def reverse_each_word(string)
   #new_arr.join(" ")
 
   #version of method using .collect
-  #arr.collect do |word|
-  #  new_arr<<word.reverse
-  #end
-  #new_arr.join(" ")
-  string.split.collect {|word| word.reverse}.join (" ")
+  arr.collect do |word|
+    new_arr<<word.reverse
+  end
+  new_arr.join(" ")
 end
