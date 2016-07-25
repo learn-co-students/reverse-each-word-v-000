3f6f74377a3d2ac34baa4544d30b59e270ae3ede
KerryAlsace <kerrydougherty@gmail.com>
2016-07-25 18:37:10 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,24 +1,24 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reversed_sentence_array = []
-  sentence_array.each do |word|
+  sentence_array.collect do |word|
     reversed_sentence_array << word.reverse
   end
-  reversed_sentence = reversed_sentence_array.join(" ")
-  reversed_sentence
+  reversed_sentence_array.join(" ")
 end
 
-
-# takes in sentence
 # sentence = "Hello there, and how are you?"
-# sentence_array = sentence.split(" ")
-# => sentence_array = ["Hello", "there,", "and", "how", "are", "you?"]
-# sentence_array.each do |word|
+# should return "olleH ,ereht dna woh era ?uoy"
+
+
+####### SAME METHOD USING .EACH INSTEAD OF .COLLECT ######
+
+# def reverse_each_word(sentence)
+#   sentence_array = sentence.split(" ")
 #   reversed_sentence_array = []
-#   reversed_sentence_array << word.reverse
+#   sentence_array.each do |word|
+#     reversed_sentence_array << word.reverse
+#   end
+#   reversed_sentence = reversed_sentence_array.join(" ")
+#   reversed_sentence
 # end
-# => reversed_sentence_array = ["olleH", ",ereht", "dna", "woh", "era", "?uoy"]
-# reversed_sentence = reversed_sentence_array.join
-# => reversed_sentence_array = "olleH ,ereht dna woh era ?uoy"
-# reversed_sentence
-# returns "olleH ,ereht dna woh era ?uoy"
