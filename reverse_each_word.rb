45412fb3004ec83cd80619f44135c762970156d8
noson cohen <natancohen23@gmail.com>
2016-09-11 15:00:29 +0000

    made #reverse_each_word using each and collect

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(string)
+  new_string = []
+  string.split.each do |word|
+    word.reverse!
+    new_string << word
+  end
+  new_string.join(" ")
+end
+
+def reverse_each_word(string)
+  new_string = []
+  string.split.collect do |word|
+    word.reverse!
+    new_string.join(" ") << word
+  end
+end
