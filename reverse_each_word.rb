af56236787a865b4dbdb24a99204f4b5a48a0d19
Aya Fushimi <aya.fushimi@gmail.com>
2016-09-13 21:18:35 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
+# def reverse_each_word(str)
+#   arr = str.split(" ")
+#   new_arr = []
+#   arr.collect{|item| new_arr << item.reverse}
+#   new_arr.join(" ")
+# end
+
 def reverse_each_word(str)
-  arr = str.split(" ")
-  new_arr = []
-  arr.collect{|item| new_arr << item.reverse}
-  new_arr.join(" ")
+  str.split.collect{|item| item.reverse}.join(" ")
 end
