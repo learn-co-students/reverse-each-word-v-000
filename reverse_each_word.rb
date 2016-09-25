351078854df6d93086c82af3c54f971733e07a17
noson cohen <natancohen23@gmail.com>
2016-09-25 18:06:18 +0000

    updated

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
 def reverse_each_word(string)
   new_string = []
-  string.split.each do |word|
+  string.split.collect do |word|
     word.reverse!
     new_string << word
   end
   new_string.join(" ")
 end
 
-def reverse_each_word(string)
-  new_string = []
-  string.split.collect do |word|
-    word.reverse!
-    new_string.join(" ") << word
-  end
-end
+#def reverse_each_word(string)
+#  new_string = []
+#  string.split.collect do |word|
+#    word.reverse!
+#    new_string.join(" ") << word
+#  end
+#end
