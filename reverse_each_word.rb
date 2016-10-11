7e15f28d7044584197eed5dac88c164f96200007
Alex Crisan <alexcrisan2005@hotmail.com>
2016-10-11 16:27:15 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ def reverse_each_word (sentence)
   sentence_array = sentence.split (" ")
   sentence_array.collect do |word|
     word.reverse
-  end
-  sentence_array.join(" ")
+  end.join(" ")
+  
 end
