1be8a83990539e5fcec9ddff268fb2f5d9a3f5de
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-10-25 17:29:31 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,9 @@
 
 
 def reverse_each_word(str)
-  rev = []
-  index = 0
+
 str1 = str.split(" ")
 str1.collect do |word|
- rev[index] = word.reverse
- index += 1
-end
-return rev.join(" ")
+  word.reverse
+end.join(" ")
 end
