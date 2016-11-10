d2d27dd35ab6202cf137ba5357e7a8d627a56b88
machen2 <victoriamachen@gmail.com>
2016-11-10 05:10:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,12 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
+
+#def reverse_each_word(string)
+#  array = string.split(" ") #change string to array
+#
+#  new_array = array.collect do |word|
+#                word.reverse
+#              end
+#  new_array.join(" ")
+#end
