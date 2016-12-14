eaf5cc3084281099b23f1490d0a6233f7ce689d3
Mariel Segovia <msegovia94@gmail.com>
2016-12-14 03:34:19 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence)
+  string = sentence.split
+  reverse_string = Array.new()
+  string.collect do |words|
+    reverse_string << words.reverse
+  end
+  reverse_string.join(" ")
+
+end
