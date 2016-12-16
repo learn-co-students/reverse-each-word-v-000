c50b37aeceb3d2aa2470d6d6e250f9c8e0cf94b5
charlie lee <ctlee9096@gmail.com>
2016-12-16 03:04:17 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+require 'pry'
+def reverse_each_word(sentence)
+  #binding.pry
+	array = sentence.split
+  #binding.pry
+  newarray = []
+  array.each do |words|
+    #binding.pry 
+    newwords = words.reverse
+    #binding.pry
+    newarray << ("#{newwords}" " ")
+    #binding.pry
+  end
+  newstring = newarray.join
+  newstring2 = newstring.chop
+  #binding.pry
+  return newstring2
+end
+
