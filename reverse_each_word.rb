777cd781af3782df432b44f5c220afcf27364f56
Angela Bardysh <angelabardysh@gmail.com>
2016-12-31 21:28:48 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(phrase)
+  array = phrase.split
+  array.each do |word|
+  word.reverse!
+end
+return array.join(" ")
+end
+
+
+def reverse_each_word(phrase)
+  array = phrase.split
+  array.collect do |word|
+    word.reverse!
+  end
+  return array.join(" ")
+end  
