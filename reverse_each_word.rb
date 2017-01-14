5f8a2188c6417856115f73445f130c4acc0ffd6f
Catherine Cordova <ccordova41@gmail.com>
2017-01-14 20:41:13 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
-def reverse_each_word(sentence1)
+#def reverse_each_word(sentence1)
 
+#  sentence1.reverse!
+#  sentence1.split.reverse.join(' ')
+#end
+
+def reverse_each_word(sentence1)
+  sentence1.split.collect{ |e| e }
   sentence1.reverse!
-  sentence1.split.reverse.join(' ')
+ sentence1.split.reverse.join(' ')
 end
