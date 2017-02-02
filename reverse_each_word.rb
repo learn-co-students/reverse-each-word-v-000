db6be192f288d6aaa824a5e487be60db9596be8f
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-02 23:58:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   final_arr = []
   arr = string.split(" ")
   arr.collect do |word|            #not sure I get it. .each worked the same, which is how
-  	final_arr.push(word.reverse)   #I woudl have done it anyway. Couldn't get .collect
+  	final_arr.push(word.reverse)   #I would have done it anyway. Couldn't get .collect
   end                           #to work how I wanted. This feels super inelegant.
   return final_arr.join(" ")
 end
