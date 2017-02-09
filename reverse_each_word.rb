e150d173e2e082334a39ecb59985388012ed8cb4
Trai Compton <tlcompton2546@outlook.com>
2017-02-09 00:38:09 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
-  reverse_array = sentence_array.collect {|word| word.reverse}
-  reverse_array.join(" ")
+  #sentence_array = sentence.split(" ")
+  #reverse_array = sentence_array.collect {|word| word.reverse}
+  #reverse_array.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
