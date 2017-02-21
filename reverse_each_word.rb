f5201c9acf1bc210088786fee60107fe9d74db20
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-02-21 20:55:12 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+#solution using .each method
+=begin
+def reverse_each_word(sentence)
+  sentence_array = []
+  sentence.split(" ").each{|word| sentence_array << word.reverse}
+  sentence_array.join(" ")
+end
+=end
+
+#solution using .collect method
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ").collect{|word| word.reverse}
+  sentence_array.join(" ")
+end
