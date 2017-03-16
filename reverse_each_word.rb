154f82cca90953726825f34c9dffa7f3e09ecb94
Liliana Alonzo <lillyneveu@gmail.com>
2017-03-16 16:26:35 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,26 @@
 require 'pry'
 
+# def reverse_each_word(string)
+#   return string.split(' ').each_with_index { |word, index| array[index] = word.reverse }.join(' ')
+# end
+
+# def reverse_each_word(string)
+#  array = string.split(' ')
+#  array.each_with_index { |word, index| array[index] = word.reverse}
+#  return array.join(' ')
+# end
+# # The above 2 examples pass all tests except for the .collect use test
+
 def reverse_each_word(string)
-  array = string.split
-  array.reverse_each do |word|
-    puts word
-  array.join
-  return array
-  end
+  return string.split(' ').collect do |word|
+    word.reverse
+  end.join(' ')
+  # you can chain from them end which will be the collected array
 end
 
-# def reverse_each_word
-#   array.collect do |word|
-#    word.reverse
-#   end
+# def reverse_each_word(string)
+#   return string.split(' ').collect do |word| word.reverse
+#   end.join(' ')
 # end
+# #The above 2 code examples pass all 3 tests
+# #The practice of linking code together as shown in ex.1 and ex.4 is called "chaining"
