d1e8136fd7d5441935703593bbb5652c9f80af47
Andrew Bonner <andrewbonner92@gmail.com>
2017-03-24 16:36:55 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,11 +12,5 @@
 #end
 
 def reverse_each_word(string)
-  new_array = Array.new
-  array = string.split
-
-  array.collect do |word|
-    new_array << word.reverse
-  end
-  new_array.join(" ")
+  string.split.collect {|word| word.reverse}.join(" ")
 end
