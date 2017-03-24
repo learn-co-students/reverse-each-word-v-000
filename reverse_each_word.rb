9f4522b6006bdb39c5670e6cad293e580134008e
Andrew Bonner <andrewbonner92@gmail.com>
2017-03-24 16:35:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+
+# SOLVED USING .EACH
+#def reverse_each_word(string)
+#  new_array = Array.new
+#  array = string.split
+#
+#  array.each do |word|
+#    new_array << word.reverse
+#  end
+#
+#  new_array.join(" ")
+#end
+
+def reverse_each_word(string)
+  new_array = Array.new
+  array = string.split
+
+  array.collect do |word|
+    new_array << word.reverse
+  end
+  new_array.join(" ")
+end
