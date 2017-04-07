78d20634ec9533a2c82364ec93b63ccdf881d1ac
Lara Matyushenko <larabrightside@gmail.com>
2017-04-07 20:06:16 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  array = sentence.split
+  new_array = []
+  array.each do |word|
+    new_array << word.reverse
+  end
+  new_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split
+  array.collect do |word|
+    word.reverse
+  end.join(" ")
+end
