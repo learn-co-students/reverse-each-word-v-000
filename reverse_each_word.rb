b689bc9c425111be1f88c87dd16cc94270c9fa80
Julia Thompson <thompson.julia@gmail.com>
2017-04-09 18:29:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,3 +14,9 @@ def reverse_each_word(sentence)
   end
   words_reversed.join(" ")
 end
+
+#solution
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
