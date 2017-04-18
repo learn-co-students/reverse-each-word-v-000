67d5a5b260f55237ace7343d863801ed1c5bad85
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-18 15:08:07 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+    new_array = []
+    new_array = string.split(" ")
+    reversed_array = []
+    new_array.collect do |word|
+
+
+    end
+    reversed_array = string.split(" ")
+    reversed_array.reverse!
+    reversed_array.join(" ")
+end
