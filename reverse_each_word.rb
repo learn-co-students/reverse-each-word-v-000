c7086ac436a36784e223e732c246efab5ff53cdc
Stephen Harrington <stephenharrington04@yahoo.com>
2017-04-24 20:41:21 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+#def reverse_each_word(string)
+  #reversed_words = []
+  #string_to_array = string.split
+  #string_to_array.each do |word|
+  #  reversed_words << word.reverse
+  #end
+  #reversed_words.join(" ")
+#end
+
+def reverse_each_word(string)
+  reversed_words = []
+  string_to_array = string.split
+  string_to_array.collect do |word|
+    reversed_words << word.reverse
+  end
+  reversed_words.join(" ")
+end
