a5b57f81ef8807ff2c40b24fcc9678456c56f5af
gb23 <gregbenj@hotmail.com>
2017-05-17 16:19:58 -0700

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+# def reverse_each_word(sentence)
+#     reverse_array = []
+#     sentence_array = sentence.split(" ")
+#     sentence_array.each {|element|
+#         reverse_array << element.reverse  
+#     }
+#     reverse_array.join(" ")
+# end
+
+def reverse_each_word(sentence)
+    sentence_array = sentence.split(" ")
+    sentence_array.collect{|element|
+        element.reverse!
+    }
+    sentence_array.join(" ")
+end
