046c876f24a8b5dc5eb3d99503dcd4d32981f58a
Dustin Anderson <dandersonsoftware@outlook.com>
2017-06-02 19:23:32 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(str)
   array_one = str.split
-  array_two = []
-  array_one.collect {|words| array_two << words.reverse}
-  array_two.join(' ')
+  array_one.collect {|words| words.reverse}
+  array_one.join(' ')
 end
