334633a9bef8b415b91959a991f0603311f40f83
Erin Doyle <erin.v.doyle@gmail.com>
2017-06-07 08:52:01 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
+#def reverse_each_word(string)
+#array = string.split(" ")
+  #array.collect do |word|
+    #word.reverse!
+  #end
+  #array.join(" ")
+#end
+
 def reverse_each_word(string)
-array = string.split(" ")
-  array.collect do |word|
-    word.reverse!
-  end
-  array.join(" ")
+  string.split.collect {|word| word.reverse}.join(" ")
 end
