3dfaa9733cd31979917b30050fbf84ecf1181ee2
Erin Doyle <erin.v.doyle@gmail.com>
2017-06-07 09:00:48 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,19 @@
+def reverse_each_word(string)
+  string.split.collect {|word| word.reverse}.join(" ")
+end
+
+#using .each
+
 #def reverse_each_word(string)
-#array = string.split(" ")
+  #string.split.each {|word| word.reverse!}.join(" ")
+#end
+
+#first solve
+
+#def reverse_each_word(string)
+#array = string.split
   #array.collect do |word|
     #word.reverse!
   #end
   #array.join(" ")
 #end
-
-def reverse_each_word(string)
-  string.split.collect {|word| word.reverse}.join(" ")
-end
