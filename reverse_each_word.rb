eadfdbe92660e3cccef471055e01536568f7afef
Eric Strickler <eric.strickler@gmail.com>
2017-06-22 16:31:38 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,11 @@ require 'pry'
 
 
 def reverse_each_word(sentence)
-  sentence_array = []
-  sentence_array << sentence.split
-    sentence_array.collect do |backwards|
-    mirror = backwards.each(&:reverse!)
-    return mirror.join " "
-  end
-end
+        sentence_array = []
+        sentence_array << sentence.split
+        sentence_array.collect do |backwards|
+          mirror = backwards.each(&:reverse!)
+          return mirror.join " "
+        end
+       sentence_array
+    end
