36ea476ed4fe5f9f0831596c1278e13b695d123c
Sohee Cho <soheecho94@gmail.com>
2017-06-28 07:07:40 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,13 @@
+# def reverse_each_word(sentence)
+#   reversed = sentence.split
+#   result = []
+#   reversed.each {|word| result<<word.reverse}
+#   result.join(" ")
+# end
+
 def reverse_each_word(sentence)
-  reversed = sentence.split
-  result = []
-  reversed.collect {|word| result<<word.reverse}
-  result.join(" ")
+  reversed = sentence.split.collect do |word|
+    word.reverse
+  end
+  reversed.join(" ")
 end
