558ebe1137fcade34ce18780539530a7994fcbea
Joanna Sese <jmgsese@gmail.com>
2017-07-17 21:12:03 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,10 +6,14 @@ def reverse_each_word(string)
   phrase.join(" ")
 end
 
+# def reverse_each_word(string)
+#   phrase = string.split
+#   phrase.collect do |word|
+#     word.reverse!
+#   end
+#   phrase.join(" ")
+# end
+#
 def reverse_each_word(string)
-  phrase = string.split
-  phrase.collect do |word|
-    word.reverse!
-  end
-  phrase.join(" ")
+  string.split.collect {|word| word.reverse!}.join(" ")
 end
