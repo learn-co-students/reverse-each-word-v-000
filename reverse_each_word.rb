ba2841a537e113937af8d522ec22505929a5bd82
Sam Ojling <s.ojling@gmail.com>
2017-08-07 08:46:26 -0700

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
-require 'pry'
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  reversed_word = sentence_array.collect do |word|
-    word.reverse
-  end
+  reversed_word = sentence_array.collect { |word| word.reverse }
   reversed_word.join(" ")
 end
