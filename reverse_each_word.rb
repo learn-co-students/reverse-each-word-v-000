2c61367b8db6d19c6d9d458d3e3905ddb4fe6d03
Jennifer Nelson <jennifernelson118@gmail.com>
2017-08-08 18:38:45 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,20 @@
+
+#/////// MY ORIGINAL CODE BEFORE I UNDERSTOOD THE POWER OF CHAINING METHODS.//////#
 # def reverse_each_word(sentence)
-#   reversed_string = [ ]
-#   sentence.each do |word|
-#     reversed_string << "#{word.reverse}"
+#   array = sentence.split
+#   string = array.collect do |word|
+#     "#{word.reverse}"
 #   end
-#   reversed_string
+#   string.join(" ")
+# end
+
+#//// BEFORE USING { } IN PLACE OF DO/END. //////#
+# def reverse_each_word(sentence)
+#   sentence.split.collect do |word| word.reverse
+#   end
+#   .join(" ")
 # end
 
 def reverse_each_word(sentence)
-  array = sentence.split
-  string = array.collect do |word|
-    "#{word.reverse}"
-  end
-  string.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
