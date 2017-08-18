d4dcbe4c5cdae5cda1a33c312a5828b026bb604f
brandon wirz <brandonwirz2@brandons-MacBook-Pro.local>
2017-08-18 15:50:06 -0600

    Done.

--- reverse_each_word_solutions.rb
+++ reverse_each_word_solutions.rb
@@ -1,13 +1,17 @@
 
 def reverse_each_word(sentence)
-  sentence.split.collect
-  {|word| word.reverse}.join(" ")
+  sentence.split.collect do |word| word.reverse}.join(" ")
+  end
 end
 
 #def reverse_each_word(sentence)
-#  new_array = []
+  #new_array = []
   #sentence.split.each do |word|
   #new_array << word.reverse!
 #end
   #return new_array.join(" ")
+#end
+
+#def reverse_each_word(sentence)
+  #sentence.reverse.split.reverse.join(" ")
 #end
