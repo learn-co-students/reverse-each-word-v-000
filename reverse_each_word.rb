bcf959986cc06c4d1493a2e68ceacc2e5affe615
Jameson Bass <jamesonbass@gmail.com>
2017-08-22 19:01:58 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,4 @@
-
-=begin
 def reverse_each_word(arg)
-  newArr = arg.split(' ')
-  arrOutput = []
-  newArr.each do |word|
-    arrOutput << word.reverse
-  end
-  arrOutput.join(' ')
-end
-=end
-
-def reverse_each_word(arg)
-  newArr = arg.split(' ')
-  arrOutput = []
-  newArr.collect do |word|
-    arrOutput << word.reverse
-  end
-  arrOutput.join(' ')
+  arg.split.collect do |word|
+    word.reverse.join(' ')
 end
