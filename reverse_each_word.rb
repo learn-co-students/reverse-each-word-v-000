bf44f6b445f08ac3972b45b97714eef85b2894d7
Cecily Downs <cecily.downs@gmail.com>
2017-08-22 19:11:46 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,22 @@
-def reverse_each_word(phrase)
-  array = phrase.split(" ")
+# REVISED "COLLECT" VERSION
 
-  new_array = array.collect do |item|
+def reverse_each_word(phrase)
+  phrase.split.collect do |item|
     item.reverse
-  end
-  new_array.join(" ")
+  end.join(" ")
 end
 
+
+# INITIAL "COLLECT" VERSION 
+#
+# def reverse_each_word(phrase)
+#   array = phrase.split(" ")
+#
+#   new_array = array.collect do |item|
+#     item.reverse
+#   end
+#   new_array.join(" ")
+# end
 
 
 # "EACH" VERSION
