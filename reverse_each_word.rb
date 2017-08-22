e9cafc3e35a6bff3bfef711481f388a958cbe142
Cecily Downs <cecily.downs@gmail.com>
2017-08-22 19:07:48 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word(phrase)
+  array = phrase.split(" ")
+
+  new_array = array.collect do |item|
+    item.reverse
+  end
+  new_array.join(" ")
+end
+
+
+
+# "EACH" VERSION
+#
+# def reverse_each_word(phrase)
+#   array = phrase.split(" ")
+#   new_array = []
+#
+#   array.each do |item|
+#     new_array << item.reverse
+#   end
+#   new_array.join(" ")
+# end
