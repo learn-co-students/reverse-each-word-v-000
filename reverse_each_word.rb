7031daad951c519112217a81122235ec29126f7b
Yomaira Escano <yjescano@gmail.com>
2017-08-28 01:39:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,7 @@
 def reverse_each_word(sentence)
-  array = sentence.split
   new_array = []
-  array.each do |word|
+  sentence.split.collect do |word|
     new_array << word.reverse
   end
   new_array.join(" ")
-end
-
-def reverse_each_word(sentence)
-  array = sentence.split
-  new_array = []
-  array.collect do |word|
-    new_array << word.reverse
-    puts new_array.join(" ")
-  end
 end
