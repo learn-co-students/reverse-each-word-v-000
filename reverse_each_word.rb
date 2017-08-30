82b0a8a15e630eb4e80cc7a1f546bdcbe78b4173
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-08-30 23:29:43 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(scentence)
+reversed_words = []
+  word_array = scentence.split
+  word_array.collect do |word|
+    reversed_words << word.reverse
+  end
+  reversed_words.join(" ")
+end
