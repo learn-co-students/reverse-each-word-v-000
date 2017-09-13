9e6409bb2ccf2de1ae2311861c676905706029ae
rjcolmed <rjcolmed@gmail.com>
2017-09-13 17:36:25 +0000

    add #reverse_each_word using #collect. will refactor later.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,20 @@
+# def reverse_each_word(phrase)
+#   conv_to_arr = phrase.split
+#   new_phrase = ""
+#   conv_to_arr.each do |word|
+#     if conv_to_arr.index(word) == 0
+#       new_phrase << "#{word.reverse}"
+#     else
+#     new_phrase << " #{word.reverse}"
+#     end
+#   end
+#   new_phrase
+# end
+
 def reverse_each_word(phrase)
-  conv_to_arr = phrase.split
-  new_phrase = ""
-  conv_to_arr.each do |word|
-    if conv_to_arr.index(word) == 0
-      new_phrase << "#{word.reverse}"
-    else
-    new_phrase << " #{word.reverse}"
-    end
+  phrase_arr = phrase.split
+  reversed = phrase_arr.collect do |word|
+    word.reverse
   end
-  new_phrase
+  reversed.join(" ")
 end
