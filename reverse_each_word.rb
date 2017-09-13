9520f300ae7ce844e5276fcf4c7406462660657c
rjcolmed <rjcolmed@gmail.com>
2017-09-13 18:43:33 +0000

    refactor #reverse_each_word

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,5 @@
-# def reverse_each_word(phrase)
-#   conv_to_arr = phrase.split
-#   new_phrase = ""
-#   conv_to_arr.each do |word|
-#     if conv_to_arr.index(word) == 0
-#       new_phrase << "#{word.reverse}"
-#     else
-#     new_phrase << " #{word.reverse}"
-#     end
-#   end
-#   new_phrase
-# end
-
+# using collect
 def reverse_each_word(phrase)
-  phrase_arr = phrase.split
-  reversed = phrase_arr.collect do |word|
-    word.reverse
-  end
-  reversed.join(" ")
+  new_phrase = phrase.split.collect { |word| word.reverse }
+  new_phrase.join(" ")
 end
