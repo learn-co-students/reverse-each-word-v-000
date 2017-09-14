7ed90850f5b5cb82403e734b3f0669bb8bb08db0
Dasha Nikitina <dashadariallc@gmail.com>
2017-09-14 10:42:09 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
   # new_array = []
-  new = sentence.split.collect { |word| word.reverse}
+  new = sentence.split.collect { |word| word.reverse}.join(" ")
 # new_array.join(" ")
-new.join(" ")
 end
