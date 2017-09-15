d318c5c1807c6b8e3942ca8edae49e05590f5243
Lois Lee <lois.lee@me.com>
2017-09-15 19:24:31 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   boom = string.split
-  final = []
-    boom.collect do |word|
-      final.push(word.reverse)
-end
-  final.join(" ")
+  candy = boom.collect do |word|
+    word.reverse
+  end
+  candy.join(" ")
 end
