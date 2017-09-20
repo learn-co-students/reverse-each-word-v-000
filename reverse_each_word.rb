72e64e50ce9263800067d19ab87774b7e369bb44
Adam Tropp <adt6261@gmail.com>
2017-09-20 15:37:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,6 @@
 def reverse_each_word(sentence)
-  reversed_sentence = ""
+
   sentence.split.collect do |word|
-    if sentence.index(word) == 0
-      reversed_sentence <<  word.reverse()
-    else
-      reversed_sentence << " " + word.reverse()
-    end
-  end
-  reversed_sentence
+    word.reverse
+  end.join(" ")
 end
