6023711bb80283b6b8346cd5ab3c9da0099a1be8
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-01 01:28:06 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 
 def reverse_each_word(sentence)
-  reversed_words = [ ]
-  sentence.split.collect do |word|
-    reversed_words << word.reverse
+  reversed_words = sentence.split.collect do |word|
+    word.reverse
   end
   reversed_words.join(" ")
 end
