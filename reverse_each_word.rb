7381a7cdab65079815f096b4a641ffd70b8df94b
Felix Lemerle <felix@lemerle.fr>
2017-10-20 23:37:49 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
-  array = string.split
 #  reverse_array = Array.new
-#  array.each { |word| reverse_array << word.reverse }
+#  string.split.each { |word| reverse_array << word.reverse }
 #  reverse_array.join(" ")
-  array.collect { |word| word.reverse }.join(" ")
+  string.split.collect { |word| word.reverse }.join(" ")
 end
