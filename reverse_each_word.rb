39d281d9b798e6601fac3da82ce724aecca0c918
Christopher Harvey <chharvey@icloud.com>
2017-10-21 23:31:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,4 @@
-def reverse_each_word(str)
-  words = str.split(" ")
-  words.collect do |word|
-  	word.reverse!
-  end
-  words.join(" ")
+def reverse_each_word (sentence)
+  sentence_array = sentence.split
+  sentence_array.collect {|word| word.reverse}.join(" ")
 end
