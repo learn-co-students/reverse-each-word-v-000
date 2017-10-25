9c792b2f930008135c90c9c6b0bc87feffab0324
tadea Simunovic <tsimunoviczg@gmail.com>
2017-10-25 01:04:02 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  reversed_array = array.collect do |word|
+    word.reverse
+  end
+  reversed_string = reversed_array.join(" ")
+  return reversed_string
+end
