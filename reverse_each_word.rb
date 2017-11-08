ec1d1f6e243bdf5f35aabc4e09a5e2d2934ff881
Neena van de Water <nkuriakose@hotmail.com>
2017-11-08 21:20:44 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,14 @@
 
-def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
-  reversed_sentence = []
-  new_array.each do | word |
-    reversed_sentence << word.reverse
-  end
-  return reversed_sentence.join(" ")
-end
+# def reverse_each_word(sentence)
+#   new_array = sentence.split(" ")
+#   reversed_sentence = []
+#   new_array.each do | word |
+#     reversed_sentence << word.reverse
+#   end
+#   return reversed_sentence.join(" ")
+# end
 
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
-  reversed_sentence = []
-  new_array.collect do | word |
-    reversed_sentence << word.reverse
-  end
-  return reversed_sentence.join(" ")
+  new_array.collect { | word | word.reverse }.join(" ")
 end
