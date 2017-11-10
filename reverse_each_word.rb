1f0411ccc99ee2ac0599eb5c57727135005ff957
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-10 05:56:14 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
-def reverse_each_word(string)
-  string.split.collect { |word| word.reverse }.join(" ")
+# def reverse_each_word(string)
+#   string.split.collect { |word| word.reverse }.join(" ")
+# end
+
+def reverse_each_word(sentence1)
+  sen1 = Array.new
+  sentence1=sentence1.split(" ")
+  sentence1.each do |word|
+    sen1 << word.reverse
+  end
+  sen1.join(" ")
 end
