28b8e0c41715c14901b453b5e12b0b93d2912251
Hannah Carnes <hecarnes@gmail.com>
2017-11-23 12:42:54 -0500

    finished

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+# def reverse_each_word(sentence)
+#   reversed_words = sentence.split.collect do |word|
+#     word.reverse
+#   end
+#   reversed_words.join(" ")
+# end
+
+def reverse_each_word(sentence)
+  sentence.split.collect(&:reverse).join(" ")
+end
