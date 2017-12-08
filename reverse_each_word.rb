de286b41131bbd8ab735934e99333f3a486518cf
Eddie Melo <heriberto@melo.nyc>
2017-12-08 14:42:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(sentence)
-  revSentence = sentence.split(' ').collect do |word|
-    word.reverse
-  end
-  revSentence.join(' ')
+  #revSentence = sentence.split(' ').collect do |word|
+    #word.reverse
+  #end
+  #revSentence.join(' ')
+  sentence.split.collect {|word| word.reverse}.join(' ')
 end
