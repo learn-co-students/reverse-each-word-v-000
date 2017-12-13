62035e58b6e77f72624dc2149d0fcb68213433b1
rh24 <ms.rebeccahong@gmail.com>
2017-12-12 20:48:20 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+# solution using .each
+def reverse_each_word(sentence)
+  reverse_string = ""
+  array = sentence.split(" ")
+    array.each do |word|
+      reverse_string += "#{word.reverse} "
+    end
+  return reverse_string.strip
+end
+
+# # solution using .collect
+def reverse_each_word(sentence)
+  string = []
+    sentence.split(" ").collect do |word|
+      string << word.reverse
+    end
+  string.join(" ")
+end
