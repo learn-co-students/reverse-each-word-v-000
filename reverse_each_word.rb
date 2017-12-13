a7256a4ca69eda9a9ca3bdb338214624c0be724f
rh24 <ms.rebeccahong@gmail.com>
2017-12-12 20:55:41 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -17,7 +17,14 @@ def reverse_each_word(sentence)
   string.join(" ")
 end
 
-# refactor
-def reverse_each_word(sentence)
+# refactor using .collect
+def reverse_each_word_using_collect(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end
+
+# refactor without iteration
+def reverse_each_word_without_iteration(sentence) # "Hello, my name is"
+  sentence.reverse.split.reverse.join(" ") # "si eman ym ,olleH" .reverse
+end                                # ["si", "eman", "ym", ",olleH"] .split
+                                   # [",olleH", "ym", "eman", "si"] .reverse
+                                   # ",olleH ym eman si" .join(" ")
