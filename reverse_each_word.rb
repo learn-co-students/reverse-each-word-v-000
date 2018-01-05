1ef470ab395cfd5a5c9bbfaddf9a25111eef4ecb
Sam Kalum <stkalum@gmail.com>
2018-01-05 15:51:03 -0500

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
 def reverse_each_word(sentence)
-  reversed_words = []
-
-  sentence.split(" ").each do |word|
-    reversed_words << word.reverse
-  end
+  reversed_words = sentence.split(" ").collect {|word| word.reverse}
 
   reversed_words.join(" ")
 end
