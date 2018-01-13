d2be9773f36b36be080189d2c9b5cbc44e8c28ab
Ingrid Wong <yw340@georgetown.edu>
2018-01-13 01:05:22 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 require "pry"
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
-  reversed=sentence_array.collect {|word| word.reverse}
-  reversed.join(" ")
+  sentence.split(" ").collect {|word| word.reverse}.join(" ")
 end
