32465face1495af8ce2b90742edbca24948bbef1
Kim Gonzales <kimgo589@gmail.com>
2018-01-25 17:41:33 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,12 @@
 
+#def reverse_each_word(string)
+#  array = string.split
+#  array.collect do |word|
+#    word.reverse
+#  end
+#  .join(" ")
+#end
+
 def reverse_each_word(string)
-  array = string.split
-  array.collect do |word|
-    word.reverse
-  end
-  .join(" ")
+  string.split.collect {| word |word.reverse}.join (" ")
 end
