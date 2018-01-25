978bdad90c69250f238fdd2c9210e03b1429f87f
Dee Race <hey@deeraceart.com>
2018-01-25 15:50:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(sentence1)
   sentence1.split.collect{|string| string.reverse()}.join(" ")
 end
+
+# Without iterating:
+
+# def reverse_each_word(sentence1)
+#   sentence1.reverse.split.reverse.join(" ")
+# end 
