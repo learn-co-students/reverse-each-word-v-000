f3b005844f0433c55d8f26dc73168a9261ee2adc
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-12 23:41:44 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,17 @@
 def reverse_each_word(sentence)
+  new_sentence_array = []
   sentence_array = sentence.split
-  sentence_array.reverse
-  
-
+    sentence_array.collect do |word|
+    new_sentence_array << word.reverse
+  end
+  new_sentence_array.join(" ")
 end
+
+# def reverse_each_word(sentence)
+#   new_array = []
+#   array = sentence.split
+#     array.collect do |word|
+#       array << word.reverse
+#     end
+#     new_array.join(" ")
+# end
