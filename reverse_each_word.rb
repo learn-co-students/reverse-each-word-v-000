d2157c7be78d685f4a38c5ceef295d0e3cb733df
Veronica Bustabad <vbustabad@gmail.com>
2018-02-15 16:38:45 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,15 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
   new_sentence_array = []
-  sentence_array.collect do |word|
+  sentence_array.each do |word|
     new_reversed_word = word.reverse!
     new_sentence_array << new_reversed_word
   end
   new_sentence_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence.split.collect do |word|
+    word.reverse!
+  end.join(" ")
 end
