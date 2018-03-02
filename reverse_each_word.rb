23f39574045b043007024807f9b22599e01087fe
Maverick Tesini <mavericktesini@gmail.com>
2018-03-02 20:11:13 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+# def reverse_each_word(string_array)
+#   y = []
+#   # string_array.reverse!
+#   x = string_array.split(" ")
+#   x.each do |word|
+#     y << word.reverse!
+#   end
+#   x.join(" ")
+# end
+
+def reverse_each_word(string_array)
+  y = []
+  # string_array.reverse!
+  x = string_array.split(" ")
+  x.collect do |word|
+    y << word.reverse!
+  end
+  x.join(" ")
+end
