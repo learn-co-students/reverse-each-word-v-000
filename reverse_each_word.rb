8c4712482973b9eeee6bbf76cda3524c4ce20a4d
Travis Williamson <travywill@gmail.com>
2018-03-06 18:52:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,7 @@ def reverse_each_word(sentence)
   reversed_words_array = []
 
   string_to_array = sentence.split(" ")
-  #array_reversed = string_to_array.reverse
-  #string_reversed = array_reversed
+  
   string_to_array.collect do |word|
     reversed_word = word.reverse
     reversed_words_array << reversed_word
