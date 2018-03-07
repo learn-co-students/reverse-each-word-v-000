b2b8fc25054bba6001076fd635b5d835d81668cc
David McGrann <dpmcgrann@gmail.com>
2018-03-07 02:27:50 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,9 @@
 def reverse_each_word(sentence)
   s2 = []
-  sentence.split.each_with_index { |w, i| s2 << "#{w.reverse}" }
+  sentence.split.each_with_index { |w, i| s2 << w.reverse }
   s2.join(" ")
 end
 
 def reverse_each_word(sentence)
-  s2 = []
-  sentence.split.collect { |w| s2 << "#{w.reverse}" }
-  s2.join(" ")
+  sentence.split.collect { |w| w.reverse }.join(" ")
 end
