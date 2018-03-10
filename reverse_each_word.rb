cb52c6d1a936b174caf092f06df3758d85493fcf
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-03-10 16:26:35 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
-def reverse_each_word(words)
-  words.each.reverse do |word|
-    puts "olleH ,ereht dna woh era ?uoy"
-  end
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
 end
 
  
