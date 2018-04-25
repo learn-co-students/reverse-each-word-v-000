bda0783b3fabb585bc48a65f401a1eb55e0c320a
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-04-25 20:22:10 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 sentence = ["Hello there, and how are you?"]
-def reverse_each_word(sentence)
-    words = sentence.split(" ").collect do |word|
-      word.reverse
+def reverse_each_word(sentence) #method
+    words = sentence.split(" ").collect do |word| #.split converts the string to an array. #collect invokes block once for each element
+      word.reverse # reversed each word, not changing the location in the sentence.
     end
-words.join(" ")
+words.join(" ") #joins objects
 end
