065dd75b040fbaaf9dfc8472c571efa0fc912a8c
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 17:53:50 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,9 @@ def reverse_each_word(sentence)
   new_sentence = new_array.collect { |word| word.reverse}
   new_sentence.join(" ")
 end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
