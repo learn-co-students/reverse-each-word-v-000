a21747cd46391bb8ddcb3fb41274d1fc901dd9e7
Carrie Savinell <carrielane89@gmail.com>
2018-05-03 20:23:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  reversed_array = []
+  sentence.split(" ").each do |words| 
+  reversed_array << words.reverse 
+  end 
+  reversed_array.join(" ")   
+end  
+
+def reverse_each_word(sentence)
+  reversed_array = []
+  sentence.split(" ").collect do |words|
+  reversed_array << words.reverse 
+  end 
+  reversed_array.join(" ")   
+end  
+
+  
+  
+  
