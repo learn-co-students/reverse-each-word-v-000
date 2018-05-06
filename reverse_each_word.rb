143d0d5c2468ab5b0edd5ff1d2b634ac9dbbe681
Dale Terrell <tragicid@gmail.com>
2018-05-06 02:28:10 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,13 @@
+require 'pry'
+
 # def reverse_each_word(string)
 #   puts reverse_each_word.each  do |string|
 #   end
 # end
 
 def reverse_each_word(string)
-  string.split.collect {|word| word.reverse.join(" ")}
+  sentence_array = string.split(" ")
+  sentence_array.collect do |w|
+    w.reverse
+  end.join(" ")
 end
