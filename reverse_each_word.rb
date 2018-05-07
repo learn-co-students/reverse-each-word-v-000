cc929daadde3993c2b92f42577bf601cbee60b49
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-07 22:12:55 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,13 @@
 def reverse_each_word(sentence)
+  sentence.split.collect {|word|
+    word.reverse}.join(" ")
+
+
 reversed_sentence= []
-reversed_sentence<< sentence.split.collect do |word|
-word.reverse
+sentence.split.each do |word|
+reversed_sentence<< word.reverse
 end
 return reversed_sentence.join(" ")
+
+
 end
