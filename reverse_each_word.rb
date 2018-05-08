5c7ba1cd971abad2449a7585ee9b945875f0a164
Hiroki Kato <hkato@sas.upenn.edu>
2018-05-08 18:05:24 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,6 @@
-def reverse_each_word(input)
-  # reverse_string = []
-  string = input.split(" ")
-  string.each do |word|
-    word.reverse 
-    # reverse_string << "#{word.reverse}"
-  end
-  # reverse_string.join(" ")
-end
+require 'pry'
 
-def reverse_each_word(input)
-  reverse_string = []
-  string = input.split(" ")
-  string.collect do |word|
-    reverse_string << "#{word.reverse}"
-  end
-  reverse_string.join(" ")
+def reverse_each_word(string)
+# binding.pry
+  string.split.collect {|word| word.reverse}.join(" ")
 end
