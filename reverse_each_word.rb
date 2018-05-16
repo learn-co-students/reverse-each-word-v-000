233eae5b354ee829ff00c9438e7361597f0867e6
theresamorelli <theresa.l.morelli@gmail.com>
2018-05-15 20:29:05 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(string)
-  str_array = string.split
-  puts "string_array: #{str_array}"
+  str_array = string.splitlearn 
   str_array.collect do |str|
     str.reverse
   end.join(" ")
