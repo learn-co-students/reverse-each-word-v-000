396524a6110703db5f6794e64fb3e44266565505
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-21 15:52:49 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,9 @@
 def reverse_each_word(phrase)
+  newphrase=""
   arph=phrase.split(" ")
-  arphn=[]
   arph.collect do |word|
-    word=word.reverse!.split("\" \\")
-      end
-    i=0
-    newphrase=""
-    while i < arph.length
-      newphrase << arph[i]
-      i+=1 
-      if i < arph.length
-        newphrase << " "
-      end
-    end
-    return newphrase
+     word.reverse!
+   end
+  arph.join(" ")
+
 end
