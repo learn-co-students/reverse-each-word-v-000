4ab5ec63f0e2c306a5a76af11517e556e9ad2ee0
Jonathan Hong <jonhong777@gmail.com>
2018-07-04 00:58:48 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
-
+require "pry"
 def reverse_each_word(sentence)
+  
   sentence_array = sentence.split(" ")
   
-  array = sentence_array.collect do |letter|
-    letter.reverse
+  array = sentence_array.collect do |word|
+    word.reverse
   end
   array.join(" ")
 end
