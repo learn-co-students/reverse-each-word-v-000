5811d4a3d4457a5e1960357375d9178ec7a6de5b
Anna Dybas <a.m.dybas@gmail.com>
2018-07-15 20:03:47 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,9 +5,9 @@
 #  new_statement.join(" ")
 # end
 
+
+
+
 def reverse_each_word(statement)
-   new_statement = []
-   array = statement.split(" ")
-   array.collect {|backwards| new_statement << backwards.reverse!}
-   new_statement.join(" ")
+   statement.split.collect {|backwards| backwards.reverse}.join(" ")
 end
