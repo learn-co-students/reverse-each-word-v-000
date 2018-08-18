84656801159e1e706815905d58d7651414ff15c8
Laura Darlak <laura@polyporedesign.com>
2018-08-18 14:54:30 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+
+# reverse each word with #each
+# def reverse_each_word(string)
+#   rev_ary = []
+#   string.split(" ").each {|word| rev_ary << word.reverse}
+#   rev_ary.join(" ")
+# end
+
+# reverse each word with #collect
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
