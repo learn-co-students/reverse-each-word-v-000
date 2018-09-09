b5b6376b73099d63fee446ac2b587f35f2abcfd6
Tim Knoll <timothy.knoll@gmail.com>
2018-09-09 03:05:05 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,11 @@
-def reverse_each_word (string)
-  word_Array = string.split(" ")
-  new_Array = []
-  new_Array.push (word_Array.collect{|word| word.reverse})
-  return new_Array.join(" ")
+
+# def reverse_each_word (string)
+#   word_Array = string.split(" ")
+#   new_Array = []
+#   new_Array.push (word_Array.collect{|word| word.reverse})
+#   return new_Array.join(" ")
+# end
+
+def reverse_each_word (sentence)
+  sentence.split.collect {|word| word.reverse}.join(' ')
 end
