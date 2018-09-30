e7e252dd8d0fb6cabacffd08f7f249c0bf4e1d6b
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-30 21:30:54 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,14 @@
-#def reverse_each_word(sentence)
- # reversed_words = []
- # sentence.split(" ").each do |word|
-  #  reversed_words.push(word.reverse)
- # end
- # reversed_words.join(" ")
-#end
+def reverse_each_word(sentence)
+  reversed_words = []
+  sentence.split(" ").each do |word|
+    reversed_words.push(word.reverse)
+  end
+  reversed_words.join(" ")
+end
 
 def reverse_each_word(sentence)
-  sentence.split(" ").collect do |word|
+  reversed = sentence.split(" ").collect do |word|
     "#{word.reverse}"
   end
+  reversed.join(" ")
 end
