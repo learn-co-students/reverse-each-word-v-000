a9df0c60ae924f0985ffc6046c776c11a00157f0
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-30 21:27:18 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+#def reverse_each_word(sentence)
+ # reversed_words = []
+ # sentence.split(" ").each do |word|
+  #  reversed_words.push(word.reverse)
+ # end
+ # reversed_words.join(" ")
+#end
+
+def reverse_each_word(sentence)
+  sentence.split(" ").collect do |word|
+    "#{word.reverse}"
+  end
+end
