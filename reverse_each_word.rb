62c1079c68af3e57a4db8f1f5cc23944b45d7e4e
Vince Andrew Pimentel <contact@vincep.me>
2018-10-14 00:30:09 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(string)
-  reversed = String.new
-  string.split(" ").collect do |word|
-    reversed += word.reverse + " "
-  end
-  return reversed.strip
+  # reversed = String.new
+  # string.split(" ").collect do |word|
+  #   reversed += word.reverse + " "
+  # end
+  # return reversed.strip
+
+  string.split(" ").collect {|word| word.reverse}.join(" ")
+  
 end
