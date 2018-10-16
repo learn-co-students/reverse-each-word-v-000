c33d6fd6a6fce109ecff74b01ed5bf0e6e0752f3
Ryan Brady <ryanbrady369@gmail.com>
2018-10-16 19:22:15 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
-  arr= string.split()
-  res = ""
-  arr.map do |curr|
-    res.concat(curr.reverse!)
+  arr = string.split()
+  arr.collect do |curr|
+    curr.reverse!
   end
-  return res
-end 
+  return arr.join(" ")
+end
