5ab1508ac8ce3406162f8cfa553cf4d09c493775
Lois Lee <lois.lee@me.com>
2018-10-22 18:51:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
-def reverse_each_word(string)
-  boom = string.split
-  boom.collect do |word|
+def reverse_each_word(sentence)
+  sentence.split.collect do |word|
     word.reverse
-  end.join(" ")
+  end
+  .join(" ")
 end
