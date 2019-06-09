8a115b05ad829d23e007f3076ed8ec540d3459bd
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 21:17:34 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,4 @@
 def reverse_each_word(list)
-  answer = []
   array = list.split(" ")
-  array.collect{|word| answer.push(word.reverse)}
-  return answer.join(" ")
+  array.collect{|word| word.reverse}.join(" ")
 end
