8f17cf6d53473260bc0ca512ef9feb37ad78d7cc
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 21:18:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(list)
-  array = list.split(" ")
-  array.collect{|word| word.reverse}.join(" ")
+  #split the list into an array, reverse each word, rejoin array back into a string
+  list.split(" ").collect{|word| word.reverse}.join(" ")
 end
