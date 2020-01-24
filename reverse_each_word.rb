c975cec69d0ecf970eb73b500e1635813a4c31fd
Julie  McManus <julieannesays@gmail.com>
2020-01-24 20:16:28 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word| word.reverse
-  end
-  reversed.join(" ")
+  reversed_sentence = sentence.split.collect { |word| word.reverse }
+  reversed_sentence.join(" ")
 end
