dc4ec289e87f876758fb1532904a3c432766aff5
Lamia Haider <lamia.haider@gmail.com>
2020-02-04 02:49:42 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,19 @@
 
 def reverse_each_word(sentence)
-  normal = sentence.split(" ")
-  reversed = []
-  normal.collect do |word|
-  reversed << word.reverse
-  end
-  reversed.join(" ")
+  sentence.split.collect {|sent| sent.reverse}.join(" ")
 end
+#   normal = sentence.split(" ")
+#   reversed = []
+#   normal.collect do |word|
+#     normal.each do |word|
+#       reversed << word.reverse
+#     end
+#   end
+#   reversed.join(" ")
+# end
+#
+# def reverse_each_word2(sentence)
+#   normal = sentence.split(" ")
+#   normal.collect {|word| word.reverse}
+#   normal.join("")
+# end
